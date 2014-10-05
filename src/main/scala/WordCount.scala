558044af8eaaeca4d593e45c5574d52ab5065ac7
Inanna Malick <inanna@recursion.wtf>
2014-10-05 01:47:49 -0400

    par

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -58,64 +58,60 @@ object WordCount {
 
   def topLinks(subreddit: String): Future[LinkListing] = {
     //println(s"issue toplinks request for $subreddit")
-    val page = url(s"http://www.reddit.com/r/$subreddit/top.json") <<? Map("t" -> "all")
+    val page = url(s"http://www.reddit.com/r/$subreddit/top.json") <<? Map("limit" -> "25", "t" -> "all")
     Http(page OK dispatch.as.json4s.Json).map(LinkListing.fromJson(subreddit)(_))
       .andThen{ case x => println(s"got ${x.map(_.links.length)} toplinks for $subreddit")}
   }
 
   def comments(link: Link): Future[CommentListing] = {
     //println(s"issue comments request for thread ${link.id} in ${link.subreddit}")
-    val page = url(s"http://www.reddit.com/r/${link.subreddit}/comments/${link.id}.json") <<? Map("depth" -> "20", "limit" -> "2000")
+    val page = url(s"http://www.reddit.com/r/${link.subreddit}/comments/${link.id}.json") <<? Map("depth" -> "25", "limit" -> "2000")
     Http(page OK dispatch.as.json4s.Json).map(CommentListing.fromJson)
-      .andThen{ case x => println(s"got ${x.map(_.comments.length)} comments for thread ${link.id} in ${link.subreddit}")}
+      .andThen{ case x => println(s"\tgot ${x.map(_.comments.length)} comments for thread ${link.id} in ${link.subreddit}")}
   }
 
   def popularWords(w: WordCount): WordCount = {
-    w.filter{ case (_,n) => n >= 200 }
+    w.filter{ case (_,n) => n >= 1000 }
   }
-
-
 
   def merge(a: WordCount, b: WordCount): WordCount =
     a.foldLeft(b){ case (wc, (s, c))  => wc.updated(s, c + wc.getOrElse(s, 0L)) }
 
-
-  //issue all futures simultaneously, because mapfuture doesn't (may be a result of mat. settings)
-  def mapConcatFuture[T, S](in: Flow[T])(f: T => Seq[Future[S]]): Flow[S] =
-    in.mapFuture( t => Future.sequence(f(t)) ).mapConcat(identity)
-
   def buildStream(subreddits: Vector[String]): Flow[WordCount] = {
-    Flow(subreddits) // Flow[String]
-      .mapFuture(topLinks) // Flow[TopPosts]
-    //mapConcatFuture(x)( ll => ll.links.map(comments)) //Flow[CommentListing]
+    Flow(subreddits)
+      .mapFuture(topLinks)
       .mapConcat(_.links)
       .mapFuture(comments)
       .map( c => c.comments.map(countWords).fold(Map(): WordCount)(merge) ) // Flow[WordCount]
-      .conflate(identity, merge) // Flow[WordCount]
   }
 
-  def run(subreddits: Seq[String]) = {
-    //todo: run 1 stream per subreddit, trivial parallel example
-    ???
+
+  def consumeStreams(in: Seq[Flow[WordCount]]) = {
+    val out = Agent(Map(): WordCount)
+
+    Future.sequence(in.map(consumeStream(1 seconds)(out))).map( _ => out.get)
   }
 
-  def consumeStream(interval: FiniteDuration)(in: Flow[WordCount]): Future[WordCount] = {
-    val out: Agent[WordCount] = Agent(Map.empty[String, Long])
+  def consumeStream(interval: FiniteDuration)(out: Agent[WordCount])(in: Flow[WordCount]): Future[Unit] = {
+
 
     val ticks = Flow(interval, interval, () => Tick)
-    in.zip(ticks.toPublisher).foreach{ case (wordcount, _) =>
+    in.foreach{ case (wordcount) =>
+      //in.zip(ticks.toPublisher).foreach{ case (wordcount, _) =>
       println("send off update with total words: " + wordcount.values.sum)
       out.alter(merge(wordcount, _) ) //send update off. would've used redis but was too lazy
-    }.map( _ => out.get() ) //foreach returns future[Unit],
+    } //foreach returns future[Unit],
   }
 
   def main(args: Array[String]): Unit = {
 
     val start = System.currentTimeMillis()
 
-    val subreddits = Vector("LifeProTips","explainlikeimfive","Jokes","askreddit","tifu", "writingprompts", "Showerthoughts")
+    val subreddits = Vector("LifeProTips","explainlikeimfive","Jokes","askreddit")
 
-    consumeStream(5 seconds)(buildStream(subreddits)).onComplete{ x =>
+    val flows = subreddits.map(s => buildStream(Vector(s)))
+
+    consumeStreams(flows).onComplete{ x =>
       println(s"${x.map(_.size)} distinct words, ${x.map(_.values.sum)} words, most popular:\n${x.map(popularWords(_))}")
       val end = System.currentTimeMillis()
       println(s"total time elapsed: ${end - start}")
