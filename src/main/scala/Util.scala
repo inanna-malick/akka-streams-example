49918c3cb4eaf07a648fbc7b8b68a6a4374d53c3
Paul Kinsky <pkinsky@gmail.com>
2014-10-12 00:29:32 -0400

    store per-subreddit wordcount

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -5,7 +5,9 @@ import scala.concurrent._
 import scala.util.{Success, Failure}
 import scala.collection.immutable._
 import akka.agent.Agent
-
+import scalaz._
+import Scalaz._
+import Util._
 
 object Util {
   def timedFuture[T](name: String)(f: Future[T])(implicit ec: ExecutionContext): Future[T] = {
@@ -21,27 +23,38 @@ object Util {
     }
   }
 
-  def writeTsv(wordcount: Map[String, Int]) = {
+  def writeTsv(fname: String, wordcount: Map[String, Int]) = {
     import java.nio.file.{Paths, Files}
     import java.nio.charset.StandardCharsets
 
     val tsv = wordcount.toList.sortBy{ case (_, n) => n }.reverse.map{ case (s, n) => s"$s\t$n"}.mkString("\n")
-    Files.write(Paths.get("words.tsv"), tsv.getBytes(StandardCharsets.UTF_8))
+    Files.write(Paths.get(fname), tsv.getBytes(StandardCharsets.UTF_8))
   }
+
+  type WordCount = Map[String, Int]
+  type Subreddit = String  
 }
 
+
+class KVStore(implicit val ec: ExecutionContext) {
+  private val commentCount: Store[Map[Subreddit,WordCount]] = new Store
+
+  def addWords(subreddit: Subreddit, words: WordCount): Future[Unit] = {
+    commentCount.update(Map(subreddit -> words))
+  }
+
+  def wordCounts: Future[Map[Subreddit, WordCount]] = commentCount.read
+}
 
 
 // jury-rigged value store starting with `zero` and merging in new elements as provided
 // could use an implicit Monoid, but then I'd need to pull in scalaz which is excessive
-case class Store[T](zero: T)(f: (T, T) => T )(implicit val ec: ExecutionContext) {
-  private val store: Agent[T] = Agent(zero)
-
-  // add the wordcount to the current one
+class Store[T](implicit val m: Monoid[T], implicit val ec: ExecutionContext) {
+  private val store: Agent[T] = Agent(m.zero)
+  
   def update(in: T): Future[Unit] =
-    store.alter(f(_, in)).map( _ => () )
+    store.alter(m.append(_, in)).map( _ => () )
 
-  // get the value of `store` after all queued updates have been completed
   def read: Future[T] =
     store.future()
 }
