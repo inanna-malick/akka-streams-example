662b5ffdf30700d1212f0ebda40a30f77c06bc64
Inanna Malick <inanna@recursion.wtf>
2014-12-12 13:28:41 -0500

    update throttle graph

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -16,7 +16,7 @@ import java.nio.charset.StandardCharsets
 object Util {
   def timedFuture[T](name: String)(f: Future[T])(implicit ec: ExecutionContext): Future[T] = {
     val start = System.currentTimeMillis()
-    println(s"--> started $name at instant $start")
+    println(s"--> started $name")
     f.andThen{
       case Success(t) =>
         val end = System.currentTimeMillis()
